77ce9c1f158cb4ff382ab4cfcdc6c21b8dd60ff0
alexRicc2 <alex.65.santanna@gmail.com>
2023-03-28 18:31:59 -0300

    theme

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Main template  file
+@package Aquiola
+
+*/
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>My theme</title>
+</head>
+<body>
+  Hello wordpress
+  
+</body>
+</html>
